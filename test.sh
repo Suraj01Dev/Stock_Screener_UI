89dd513bcba3d7166f6107be8134e13469b2dba3
suraj-14258 <suraj-14258@suraj-14258.csez.zohocorpin.com>
2024-02-09 07:51:16 +0530

    adding test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pylint my_library -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt
+pylint stock_app.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt 
 
