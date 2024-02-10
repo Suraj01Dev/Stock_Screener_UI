f0b67931fe58ecf09b690dbb0fab3b721ce34c91
suraj-14258 <suraj-14258@suraj-14258.csez.zohocorpin.com>
2024-02-10 11:57:32 +0530

    added test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bandit --format json --output bandit-report.json --recursive stock_app.py
-pylint stock_app.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt 
+bandit --format json --output bandit-report.json --recursive stock_app
+pylint stock_app -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt 
 exit 0
 
